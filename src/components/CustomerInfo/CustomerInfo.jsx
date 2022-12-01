43ac71ec1dc71afcd70ac9921f7dd8ce657b95ed
Dylan Roets <dylanroets@yahoo.com>
2022-12-01 12:08:58 -0600

    started layout and css

--- src/components/CustomerInfo/CustomerInfo.jsx
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -1,7 +1,39 @@
+import './CustomerInfo.css';
+
 function CustomerInfo(){
     return(
         <>
-        <h1>Step 2: Customer Info</h1>
+        <h2>Customer Info</h2>
+        <form id='customerInputs'>
+            <input
+                className='inputFields'
+                type="text"
+            //   value={name}
+            //   onChange={}
+                placeholder="Name"
+            /><br/>
+            <input
+                className='inputFields'
+                type="text"
+            //   value={name}
+            //   onChange={}
+                placeholder="Street Address"
+            /><br/>
+            <input
+                className='inputFields'
+                type="text"
+            //   value={name}
+            //   onChange={}
+                placeholder="City"
+            /><br/>
+            <input
+                className='inputFields'
+                type="text"
+            //   value={name}
+            //   onChange={}
+                placeholder="Zip"
+            /><br/>
+        </form>
         </>
     )
 }
