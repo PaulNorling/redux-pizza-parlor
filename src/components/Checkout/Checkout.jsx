74b4cf7af79108afbcf8d3a1fc5b926ee98162ca
AnthonyDampier <Dampieranthony2@gmail.com>
2022-12-01 11:06:23 -0600

    much of jsx for checkout is scoped

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+
 function Checkout(){
+    const [name, setName] = useState('NAME PLACEHOLDER');
+    const [streetAddress, setStreeAddress] = useState('555 Applewood Lane');
+
+
     return(
         <>
         <h1>Step 3: Checkout</h1>
+        <div id="checkout-info">
+            <div>
+                <div id="user-info">
+                    <p id="user-name">{name}</p>
+                    <p id="user-street-address">{streetAddress}</p>
+                    <p id="user-city-state">Minneapolis, MN</p>
+                </div>
+                <h2 id="deliver-takeout">For Delivery</h2>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Cost</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        
+        </div>
         </>
     )
 }
