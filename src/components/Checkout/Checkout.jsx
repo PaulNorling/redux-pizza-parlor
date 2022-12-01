33a3d9452a3ed0151bc0baeef3ebe9cd77945d17
AnthonyDampier <Dampieranthony2@gmail.com>
2022-12-01 13:58:26 -0600

    useEffect and totalCalc

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Checkout(){
 
-    // const setVariable = (newInfo) => {
-    //     setName()
-    // }
+    
 
     const [name, setName] = useState('NAME PLACEHOLDER');
     const [streetAddress, setStreetAddress] = useState('555 Applewood Lane Placeholder');
@@ -14,19 +12,19 @@ function Checkout(){
 
     const [type, setDeliverOrTakeout] = useState('For Deliver')
 
-    const [checkoutItemsArray, setCheckoutItemArray] = useState(
+    const checkoutItemsArray =
         [
             {name:'pep', price: 2.12, quantity: 1}, 
             {name:'pep1', price: 3.12, quantity: 1},
             {name:'pep2', price: 4.12, quantity: 1},
             {name:'pep3', price: 5.12, quantity: 1}
-        ]
-    )
+        ];
+
     const [total, setTotal] = useState(0);
         
-    const calcTotal = (checkoutItemsArray) => {
+    const calcTotal = () => {
         let total = 0;
-        checkoutItemsArray.map((item, index) => {
+        checkoutItemsArray.map(item =>{
             total += item.price;
         })
         setTotal(total);
@@ -54,43 +52,50 @@ function Checkout(){
 
     }
 
+    // on page load dom will retrieve info from store and set values.
+    useEffect(() => {
+        console.log('Inside useEffect, calc total');
+        //
+        calcTotal();
+    }, []);
+
 
 
     return(
         <>
-        <h1>Step 3: Checkout</h1>
-        <div id="checkout-info">
-            <div>
-                <div id="user-info">
-                    <p id="user-name">{name}</p>
-                    <p id="user-street-address">{streetAddress}</p>
-                    <p id="user-city-state">{cityState}</p>
+            <h1>Step 3: Checkout</h1>
+            <div id="checkout-info">
+                <div>
+                    <div id="user-info">
+                        <p id="user-name">{name}</p>
+                        <p id="user-street-address">{streetAddress}</p>
+                        <p id="user-city-state">{cityState}</p>
+                    </div>
+                    <h2 id="deliver-takeout">{type}</h2>
                 </div>
-                <h2 id="deliver-takeout">{type}</h2>
-            </div>
-            <table id='user-order-recipe'>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Cost</th>
-                    </tr>
-                </thead>
+                <table id='user-order-recipe'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Cost</th>
+                        </tr>
+                    </thead>
 
-                <tbody>
-                    {console.log(checkoutItemsArray)}
-                    {checkoutItemsArray.map((item, index) => {
-                        return(
-                            <tr key={index}>
-                                <td>{item.name}</td>
-                                <td>{item.price}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-            <h1>Total: {total}</h1>
-            <button onClick={handleCheckout}>Checkout</button>
-        </div>
+                    <tbody>
+                        {console.log(checkoutItemsArray)}
+                        {checkoutItemsArray.map((item, index) => {
+                            return(
+                                <tr key={index}>
+                                    <td>{item.name}</td>
+                                    <td>{item.price}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                <h1>Total: {total}</h1>
+                <button onClick={handleCheckout}>Checkout</button>
+            </div>
         </>
     )
 }
